a52f84040c6a7696cd839f6e89fe7ed6c8d417d8
Byron Smith <smithby1@msu.edu>
2011-07-18 16:57:54 -0400

    Updated fasttree_hpcc_job.sh to more easily run.
    Now you don't have to change the script itself every time you want
    to run it on a different file, or with a different output.  Carefull
    though!  It could not work without telling you, and still submit the
    job to the HPCC.

--- fasttree_hpcc_job.sh
+++ fasttree_hpcc_job.sh
@@ -1,25 +1,14 @@
 #!/usr/bin/env bash
-#PBS -l walltime=168:00:00,mem=4gb -M smithby1@msu.edu -m abe -N 20110711_1 -r y
+#PBS -l walltime=168:00:00,mem=4gb -M smithby1@msu.edu -m abe -N FastTree -r y
 #
-# Version 0.1
-# A bash script to run FastTree as an automated job on the hpcc on a
-#+ non-interactive node.
-#
-# Run like so:
-#   > qsub fasttree_hpcc_job.sh
-#
-# Version dated: 2011-07-14
-# Run#1: 2011-11-10
-# Run#2: 2011-11-14 (made it a universal script)
+# Run FastTree as a non-interactive job
+# 
+# Take arguments for the input alignment and the output file name.
+#+ Expects FastTree to be found at "~/Projects/eisen/bin/fasttree",
+#+ which may or not be true on other systems (TODO).
 
-
-cd ~/Projects/eisen/rpoB_newreps
-OUTFILE=fasttree.err
-echo "-------------------" >$OUTFILE
-echo "Directory: $PWD" >$OUTFILE
-echo "Timepoint: `date`" >$OUTFILE
-echo "Job Name: 20110711_1"  >$OUTFILE
-echo "\#PBS -l walltime=168:00:00,mem=4gb -M smithby1@msu.edu -m abe -N 20110711_2 -r y" >$OUTFILE
-echo 'Starting FastTree with the following command:' >$OUTFILE
-echo '"../bin/FastTree rpoB.trim.ali"' >$OUTFILE
-../bin/FastTree rpoB.lek0.60.filt.gt1.lek0.80.reps2.ali >rpoB.lek0.60.filt.gt1.lek0.80.reps2.untrimmed.tre 2>$OUTFILE
+FT_PATH_DEFAULT="~/Projects/eisen/bin/fasttree"
+infile=$1
+outfile=$2
+STDERR_REDIRECTION_DEFAULT="`dirname outfile`.err"
+FT_PATH_DEFAULT $infile >$outfile 2>$STDERR_REDIRECTION_DEFAULT
